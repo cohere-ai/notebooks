078720e3c8a7d3a63f3cfbb85122b639fe6ec38c
Anirudh31415926535 <anirudh@cohere.com>
2024-11-19 15:44:25 +0800

    Add sql agent i-5O (#242)

---
+++ notebooks/agents/i-5O-sql-agent/status.sql
@@ -0,0 +1,60 @@
+CREATE TABLE status (
+    timestamp_event TEXT,
+    timezone TEXT,
+    date TEXT,
+    hour TEXT,
+    minute TEXT,
+    station_name TEXT,
+    station_status TEXT
+);
+
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-09 19:28:00', 'Canada/Toronto', '2024-05-09', '19', '28', 'stn3', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-21 06:57:00', 'Canada/Toronto', '2024-04-21', '6', '57', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-11 23:52:00', 'Canada/Toronto', '2024-04-11', '23', '52', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-03 21:52:00', 'Canada/Toronto', '2024-04-03', '21', '52', 'stn2', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-30 05:01:00', 'Canada/Toronto', '2024-04-30', '5', '1', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-12 21:43:00', 'Canada/Toronto', '2024-04-12', '21', '43', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-29 09:27:00', 'Canada/Toronto', '2024-05-29', '9', '27', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-17 18:43:00', 'Canada/Toronto', '2024-05-17', '18', '43', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-03 04:50:00', 'Canada/Toronto', '2024-04-03', '4', '50', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-18 03:24:00', 'Canada/Toronto', '2024-04-18', '3', '24', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-28 08:25:00', 'Canada/Toronto', '2024-05-28', '8', '25', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-28 14:59:00', 'Canada/Toronto', '2024-04-28', '14', '59', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-17 02:13:00', 'Canada/Toronto', '2024-05-17', '2', '13', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-10 03:01:00', 'Canada/Toronto', '2024-05-10', '3', '1', 'stn2', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-29 17:25:00', 'Canada/Toronto', '2024-04-29', '17', '25', 'stn4', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-06 13:30:00', 'Canada/Toronto', '2024-05-06', '13', '30', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-10 20:15:00', 'Canada/Toronto', '2024-05-10', '20', '15', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-23 23:25:00', 'Canada/Toronto', '2024-05-23', '23', '25', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-30 12:40:00', 'Canada/Toronto', '2024-05-30', '12', '40', 'stn5', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-01 06:08:00', 'Canada/Toronto', '2024-04-01', '6', '8', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-24 10:50:00', 'Canada/Toronto', '2024-04-24', '10', '50', 'stn1', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-05 00:58:00', 'Canada/Toronto', '2024-05-05', '0', '58', 'stn5', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-28 08:27:00', 'Canada/Toronto', '2024-05-28', '8', '27', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-04 00:47:00', 'Canada/Toronto', '2024-05-04', '0', '47', 'stn5', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-10 21:26:00', 'Canada/Toronto', '2024-05-10', '21', '26', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-07 14:13:00', 'Canada/Toronto', '2024-05-07', '14', '13', 'stn3', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-26 22:13:00', 'Canada/Toronto', '2024-04-26', '22', '13', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-15 09:59:00', 'Canada/Toronto', '2024-04-15', '9', '59', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-02 06:48:00', 'Canada/Toronto', '2024-05-02', '6', '48', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-04 04:22:00', 'Canada/Toronto', '2024-04-04', '4', '22', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-29 03:12:00', 'Canada/Toronto', '2024-04-29', '3', '12', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-28 01:41:00', 'Canada/Toronto', '2024-04-28', '1', '41', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-23 05:27:00', 'Canada/Toronto', '2024-05-23', '5', '27', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-20 10:57:00', 'Canada/Toronto', '2024-04-20', '10', '57', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-03 00:56:00', 'Canada/Toronto', '2024-05-03', '0', '56', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-22 12:40:00', 'Canada/Toronto', '2024-05-22', '12', '40', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-08 08:36:00', 'Canada/Toronto', '2024-04-08', '8', '36', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-14 23:14:00', 'Canada/Toronto', '2024-05-14', '23', '14', 'stn3', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-02 23:40:00', 'Canada/Toronto', '2024-05-02', '23', '40', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-19 19:11:00', 'Canada/Toronto', '2024-05-19', '19', '11', 'stn2', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-07 16:55:00', 'Canada/Toronto', '2024-05-07', '16', '55', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-02 01:27:00', 'Canada/Toronto', '2024-04-02', '1', '27', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-01 18:01:00', 'Canada/Toronto', '2024-04-01', '18', '1', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-02 05:31:00', 'Canada/Toronto', '2024-05-02', '5', '31', 'stn4', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-26 16:37:00', 'Canada/Toronto', '2024-05-26', '16', '37', 'stn5', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-06 21:10:00', 'Canada/Toronto', '2024-04-06', '21', '10', 'stn1', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-29 05:15:00', 'Canada/Toronto', '2024-05-29', '5', '15', 'stn5', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-30 03:18:00', 'Canada/Toronto', '2024-05-30', '3', '18', 'stn2', 'productive');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-04-19 19:31:00', 'Canada/Toronto', '2024-04-19', '19', '31', 'stn4', 'downtime');
+INSERT INTO status (timestamp_event, timezone, date, hour, minute, station_name, station_status) VALUES ('2024-05-10 06:17:00', 'Canada/Toronto', '2024-05-10', '6', '17', 'stn1', 'productive');
